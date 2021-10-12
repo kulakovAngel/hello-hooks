7743f57b9458b0c2fed6b267fd394c94c2f4a04c
ValeryKulakov <kulakov.resurs@tut.by>
2021-10-12 23:00:08 +0300

    Clean

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import {useState} from 'react';
-
 export const App = () => {
-  const [isActive, setActive] = useState(false);
-  
-  const colorControl = () => {
-    setActive(!isActive)
-  }
-
-  return (
-    <div onClick={colorControl} style={{color: isActive && 'red'}}>
-      Hello, function components!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, function components!
+        </div>
+    );
 };
